84d1db9b516b5956825ecbe59075d2d73341032d
Zecheru Ionut <56791024+izecheru@users.noreply.github.com>
2024-06-06 12:38:24 +0300

    we don't do that here (switch)

--- QA_Task/Program.cs
+++ QA_Task/Program.cs
@@ -34,64 +34,63 @@ namespace QA_Task
 
             MyLogger.CreateLogFile();
 
-            switch (args.Length)
+            if(args.Length==3)
             {
-                case 3:
-                    try
+                try
+                {
+                    if (!int.TryParse(args[1], out maxLifetime))
                     {
-                        if (!int.TryParse(args[1], out maxLifetime))
-                        {
-                            ConsolePrinter.PrintError($"[{args[1]}] wrong format");
-                            Environment.Exit(1);
-                        }
-                        if (!int.TryParse(args[2], out monitoringFreq))
-                        {
-                            ConsolePrinter.PrintError($"[{args[2]}] wrong format");
-                            Environment.Exit(1);
-                        }
-
-                        // we start a new thread for the keyboard listener so we 
-                        // don't use sleep on this one and risk to not be able to 
-                        // send input to the program
-                        Thread quitListener = new Thread(ListenForKey);
-                        quitListener.Start();
-
-                        ConsolePrinter.PrintInfo("Listening...");
-                        monitoringFreqMilliseconds = ConvertToMilliseconds(monitoringFreq);
-                        string targetProc = args[0].ToLower();
-
-                        // run as long as we dont press q
-                        while (!quit)
-                        {
-                            ConsolePrinter.PrintInfo($"checking for running processes with name: {targetProc}");
-                            Process[] targets = Process.GetProcessesByName(targetProc);
-
-                            // print found processes 
-                            targets.PrintProcesses();
-                            // kill the processes that have a runtime bigger or equal to the maxLifetime variable
-                            targets.KillProcesses(maxLifetime);
-
-                            for (int i = 0; i < monitoringFreqMilliseconds / 100; i++)
-                            {
-                                if (quit)
-                                    break;
-                                Thread.Sleep(100);
-                            }
-
-                            // a variable to know how many cycles of checking for a process we've been through
-                            // not used at the moment, added for debugging purposes
-                            ++cicles;
-                        }
+                        ConsolePrinter.PrintError($"[{args[1]}] wrong format");
+                        Environment.Exit(1);
                     }
-                    catch (Exception ex)
+                    if (!int.TryParse(args[2], out monitoringFreq))
                     {
-                        Console.WriteLine(ex.ToString());
+                        ConsolePrinter.PrintError($"[{args[2]}] wrong format");
+                        Environment.Exit(1);
                     }
-                    break;
-                default:
-                    Console.WriteLine("eg usage: ./QA_Task processName maxLifetime frequency");
-                    Console.WriteLine("\nprocessName <string>:\tThe name of the process that must be killed\nmaxLifetime <int>:\tThe amount of minutes a process is allowed to live\nfrequency <int>:\tThe frequency in minutes at which we verify the processes");
-                    break;
+
+                    // we start a new thread for the keyboard listener so we 
+                    // don't use sleep on this one and risk to not be able to 
+                    // send input to the program
+                    Thread quitListener = new Thread(ListenForKey);
+                    quitListener.Start();
+
+                    ConsolePrinter.PrintInfo("Listening...");
+                    monitoringFreqMilliseconds = ConvertToMilliseconds(monitoringFreq);
+                    string targetProc = args[0].ToLower();
+
+                    // run as long as we dont press q
+                    while (!quit)
+                    {
+                        ConsolePrinter.PrintInfo($"checking for running processes with name: {targetProc}");
+                        Process[] targets = Process.GetProcessesByName(targetProc);
+
+                        // print found processes 
+                        targets.PrintProcesses();
+                        // kill the processes that have a runtime bigger or equal to the maxLifetime variable
+                        targets.KillProcesses(maxLifetime);
+
+                        for (int i = 0; i < monitoringFreqMilliseconds / 100; i++)
+                        {
+                            if (quit)
+                                break;
+                            Thread.Sleep(100);
+                        }
+
+                        // a variable to know how many cycles of checking for a process we've been through
+                        // not used at the moment, added for debugging purposes
+                        ++cicles;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("eg usage: ./QA_Task processName maxLifetime frequency");
+                Console.WriteLine("\nprocessName <string>:\tThe name of the process that must be killed\nmaxLifetime <int>:\tThe amount of minutes a process is allowed to live\nfrequency <int>:\tThe frequency in minutes at which we verify the processes");
             }
         }
     }
